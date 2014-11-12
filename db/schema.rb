eb59ccaed8ef4f9f91638eafb1b259ccb1efb2e7
Ikuo Terado <eqobar@gmail.com>
2014-11-12 22:53:51 +0900

    Add note model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141110152005) do
+ActiveRecord::Schema.define(version: 20141112130052) do
+
+  create_table "notes", force: true do |t|
+    t.string   "title",      limit: 255
+    t.text     "content",    limit: 65535
+    t.integer  "user_id",    limit: 4
+    t.datetime "created_at",               null: false
+    t.datetime "updated_at",               null: false
+  end
 
   create_table "users", force: true do |t|
     t.string   "email",                  limit: 255, default: "", null: false
---
+++ spec/factories/notes.rb
@@ -0,0 +1,8 @@
+FactoryGirl.define do
+  factory :note do
+    title "MyString"
+content "MyText"
+user_id 1
+  end
+
+end
